deb2a8dce01de11c79b957177214fcf9e78106f8
Pablo Caño <pablocpascual@gmail.com>
2024-06-24 09:20:36 +0200

    Añadir icono a la aplicación

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import sys
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine
+from PySide6.QtGui import QIcon
 from video_handler import VideoHandler
+import ctypes
 
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
+
+    myappid = 'mycompany.myproduct.subproduct.version' # arbitrary string
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+
+    app.setApplicationName("Video Player")
+    app.setWindowIcon(QIcon("./images/icono.png"))
     video_handler = VideoHandler()
     engine.rootContext().setContextProperty("videoHandler", video_handler)
     engine.load('main.qml')
